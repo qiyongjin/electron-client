0b8653e35029ac0829d279527632386b902c0730
qiyongjin <3131471367@qq.com>
2026-09-08 08:47:45 +0800

    feat: 退出时清空mainwindow

--- src/main/windowManager.ts
+++ src/main/windowManager.ts
@@ -16,39 +16,40 @@ export function createMainWindow(): BrowserWindow {
     minHeight: 600,
     webPreferences: {
       nodeIntegration: false,  // 避免渲染进程直接访问 Node.js
-      contextIsolation: true,
+      contextIsolation: true,  // 确保渲染进程和主进程的上下文隔离
       preload: preloadPath,
       webSecurity: isDev // 禁用浏览器的同源策略（CORS 限制），允许你的渲染进程随意请求任何跨域资源  
     },
     show: false,
   };
 
-  const mainWindow = new BrowserWindow(options);
+  let mainWindow = new BrowserWindow(options) as BrowserWindow | null;
 
   if (isDev) {
-    mainWindow.loadURL('http://localhost:3000').catch(err => {
+    mainWindow?.loadURL('http://localhost:3000').catch(err => {
       console.error('❌ Failed to load URL:', err);
     });
   } else {
     const filePath = join(__dirname, '../../renderer/index.html');
     console.log('📁 Loading production file:', filePath);
-    mainWindow.loadFile(filePath).catch(err => {
+    mainWindow?.loadFile(filePath).catch(err => {
       console.error('❌ Failed to load file:', err);
     });
   }
 
-  mainWindow.once('ready-to-show', () => {
+  mainWindow?.once('ready-to-show', () => {
     mainWindow?.show();
 
     // 开发环境下打开开发者工具
     if (isDev) {
-      mainWindow.webContents.openDevTools();
+      mainWindow?.webContents.openDevTools();
     }
   });
 
-  mainWindow.on('closed', () => {
+  mainWindow?.on('closed', () => {
     console.log('❌ Window closed');
+    mainWindow = null;
   });
 
-  return mainWindow;
+  return mainWindow! as BrowserWindow;
 }
